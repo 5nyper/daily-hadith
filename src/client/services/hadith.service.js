40e01f1d7dc5e28422d4c957a19e83c17d531846
Karim al-Laythi <ibnsenju@IbnSenjus-CNS.local>
2023-01-07 00:24:22 -0500

    add advised fallback logic

--- src/client/services/hadith.service.js
+++ src/client/services/hadith.service.js
@@ -1,25 +1,39 @@
 import axios from "axios"
 
 export async function getHadith() {
-    const books = ["abudawud", "bukhari", "ibnmajah", "muslim", "mailik", "tirmidhi", "nasai"]
-    const axiosObj = axios.create()
+    const books = ["abudawud", "bukhari", "ibnmajah", "muslim", "malik", "tirmidhi", "nasai"]
 
     const selectedBook = books[Math.floor((Math.random() * books.length))]
 
 
-    let englishHadith = await axiosObj
-        .get(`https://cdn.jsdelivr.net/gh/fawazahmed0/hadith-api@1/editions/eng-${selectedBook}.json`)
-        .then((responseBody) => {
-            return responseBody.data
-        })
-    let arabicHadith = await axiosObj
-        .get(`https://cdn.jsdelivr.net/gh/fawazahmed0/hadith-api@1/editions/ara-${selectedBook}.json`)
-        .then((responseBody) => {
-            return responseBody.data
-        })
+    let englishHadith = await fetchWithFallback([
+        "https://cdn.jsdelivr.net/gh/fawazahmed0/hadith-api@1/editions/eng-",
+        "https://raw.githubusercontent.com/fawazahmed0/hadith-api/1/editions/eng-",
+    ], selectedBook)
+    let arabicHadith = await fetchWithFallback([
+        "https://cdn.jsdelivr.net/gh/fawazahmed0/hadith-api@1/editions/ara-",
+        "https://raw.githubusercontent.com/fawazahmed0/hadith-api/1/editions/ara-",
+    ], selectedBook)
     const randomNum = Math.floor(Math.random() * englishHadith.hadiths.length)
 
 
 
     return [englishHadith.hadiths[randomNum], arabicHadith.hadiths[randomNum], selectedBook + " | " + englishHadith.metadata.sections[englishHadith.hadiths[randomNum].reference.book.toString()]]
+}
+
+const fetchWithFallback = async (links, selectedBook) => {
+    const axiosObj = axios.create()
+    let response;
+    for (let link of links) {
+        try {
+            response = await axiosObj
+                .get(`${link}${selectedBook}.min.json`)
+
+            return response.data
+
+        } catch (e) {
+            console.log("err")
+        }
+    }
+    return response
 }
